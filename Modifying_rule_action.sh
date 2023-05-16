9e2a96620b05c5eaa38c8a01c6abe5abbd2669d7
Wael_196 <Waelessam18@gmail.com>
2023-05-16 18:33:57 +0300

    modifying rule

---
+++ Modifying_rule_action.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Author: Dina Hassan
+# Email: dhassan@vmware.com
+fqdn="vip-nsx-mgmt.vrack.vsphere.local"
+user="admin"
+password="VMware123!VMware123!"
+file="ports2.txt"
+policy=$1
+action=\"$2\"
+rules=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep INTEGR)
+for i in $(echo $rules ) ; 
+do newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"action\" :.*+\"action\" : $action ,+" );
+result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" );
+if [[ -z $(echo $result | grep "\"action\" :" ) ]] ; 
+then 
+echo -e "\033[1;31mCannot get rule configuration, something went wrong ! \033[0m"; 
+echo -e $result  ;
+exit 1 ;
+else  
+echo "========================================================================================"
+echo -e "\033[1;32m New action associated with rule $i : \033[0m"
+echo "========================================================================================"
+echo $result | awk -F '"action" : ' '{print $2}' | awk -F ',' '{print $1}'
+fi
+done
