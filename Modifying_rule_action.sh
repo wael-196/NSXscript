de5d1d752ff53e1c93ff8d70d067e786343ebbfc
Wael_196 <Waelessam18@gmail.com>
2023-05-16 18:40:48 +0300

    modyfing rule

--- Modifying_rule_action.sh
+++ Modifying_rule_action.sh
@@ -5,9 +5,10 @@ fqdn="vip-nsx-mgmt.vrack.vsphere.local"
 user="admin"
 password="VMware123!VMware123!"
 file="ports2.txt"
+keyword="CATCH_"
 policy=$1
 action=\"$2\"
-rules=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep INTEGR)
+rules=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep $keyword)
 for i in $(echo $rules ) ; 
 do newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"action\" :.*+\"action\" : $action ,+" );
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" );
