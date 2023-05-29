633056bbd176822f518eca66ad1204468bce6629
Wael_196 <Waelessam18@gmail.com>
2023-05-29 16:30:28 +0300

    remove services

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -169,7 +169,8 @@ if [ "$services" == "  " ] ;
 then
 newservices=${newservices:0:-1} ; 
 fi
-services="\"services\" : [$newservices $services],"
+#services="\"services\" : [$newservices $services],"
+services="\"services\" : [ \"\/infra\/services\/R_TCP_1324-2345\" ],"​
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$services+" )
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" )
 if [[ -z $(echo $result | grep "\"services\" :" ) ]] ; 
