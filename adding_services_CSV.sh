e26f9fc1f46e1a330cad110fffa375c2ade50574
Wael_196 <Waelessam18@gmail.com>
2023-05-29 03:02:03 +0300

    fff

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -24,7 +24,6 @@ destport=$(echo $i | awk -F ',' '{print $3}') ;
 # echo "destport=$destport" ;
 for t in $(echo $rule); do flow=$protocap"*"$destport"*"$t"*""\n"$flow ; done 
 done
-echo -e $flow 
 echo "========================================================================================" ;
 echo -e "\033[1;32mNon Zero Rules: \033[0m" ;
 echo "========================================================================================" ;
