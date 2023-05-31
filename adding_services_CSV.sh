a4e75990599de5c531396b1699ff04b822e30e80
Wael_196 <Waelessam18@gmail.com>
2023-06-01 01:46:50 +0300

    sss

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -182,14 +182,14 @@ if (( "$services_number" <= "$max_num" ))
 then
 yy=$newservices" "$services
 # echo $yy jjdjd
-total_service=$(echo $yy | sed 's/,//' | sed 's/ /, /' )
+total_service=$(echo $yy | sed 's/,//g' | sed 's/ /, /g' )
 services="\"services\" : [$total_service],"
 echo $total_service
 # exit 1
 else
 echo -e "\033[1;31mNumber of services has exceeded maximum size $max_num \033[0m";
 lastservices_count=$(( $services_number-$max_num ))
-first120=$(echo -e "$newservices $services" | sed 's/,//' | tr ' ' '\n' | sort | uniq | sed '/^$/d' | head -n $max_num |  tr '\n' ' ' )
+first120=$(echo -e "$newservices $services" | sed 's/,//g' | tr ' ' '\n' | sort | uniq | sed '/^$/d' | head -n $max_num |  tr '\n' ' ' )
 echo $first120
 lastservices=$(echo -e "$newservices $services"  | tr ' ' '\n' | sort | uniq | sed '/^$/d' | head -n $lastservices_count |  tr '\n' ' ' )
 services="\"services\" : [ $first120 ],"
