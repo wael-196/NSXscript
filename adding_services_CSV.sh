9e26a7bcaf97ea96e3d7e750ef40c622cd01fece
Wael_196 <Waelessam18@gmail.com>
2023-05-31 13:29:10 +0300

    ddd

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -186,7 +186,7 @@ services="\"services\" : [$newservices $services],"
 echo $services
 else
 echo -e "\033[1;31mNumber of services has exceeded maximum size 128 \033[0m";
-first120=$(echo -e "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | head -n 120 | wc -l)
+first120="$newservices $services"
 echo $first120
 lastservices_count=$(( $services_number-120 ))
 lastservices=$(echo -e "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | tail -n $lastservices_count )
