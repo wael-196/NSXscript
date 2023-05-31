68d25813ef08671bc062398cdfa14743cdf4cce6
Wael_196 <Waelessam18@gmail.com>
2023-05-31 11:54:30 +0300

    gggg

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -184,10 +184,10 @@ services="\"services\" : [$newservices $services],"
 echo $services
 else
 echo -e "\033[1;31mNumber of services has exceeded maximum size 128 \033[0m";
-first120=$(echo "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | head -n 120)
+first120=$(echo "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | head -n 120 | tr '\n' ' ')
 first120=${first120:0:-1} 
 lastservices_count=$(( $services_number-120 ))
-lastservices=$(echo "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | tail -n $lastservices_count )
+lastservices=$(echo "$newservices $services" | tr ' ' '\n' |  sort | uniq | sed '/^$/d' | tail -n $lastservices_count | tr '\n' ' ' )
 services="\"services\" : [ $first120 ],"
 fi
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$services+" )
