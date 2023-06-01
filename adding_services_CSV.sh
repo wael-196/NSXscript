cae9c11d0c3f0c1063706383778f20202516386d
Wael_196 <Waelessam18@gmail.com>
2023-06-02 02:00:43 +0300

    last good point

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -196,7 +196,8 @@ read -e -i "$new_rule" -p "Please enter the new rule name to add the remaining $
 new_rule="${input:-$new_rule}"
 new_rule_body="{\"action\" : \"ALLOW\", \"display_name\": \"$new_rule\", \"sequence_number\": 1, \"source_groups\" : [ \"ANY\" ], \"destination_groups\" : [ \"ANY\" ], \"logged\" : false, $services \"scope\" : [ \"ANY\" ]}"
 echo $new_rule_body
-result=$(curl -u $user:$password -k -X PATCH https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$new_rule -s -d "$new_rule_body" --header "Content-Type: application/json" )
+curl -u $user:$password -k -X PATCH https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$new_rule -s -d "$new_rule_body" --header "Content-Type: application/json" 
+result=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$new_rule -s  --header "Content-Type: application/json" )
 services="\"services\" : [ $first120 ],"
 if [[ -z $(echo $result | grep "\"services\" :" ) ]] ; 
 then 
