812230f46cee9fdcfff2c09667b5bf48177c5426
Wael_196 <Waelessam18@gmail.com>
2023-06-04 00:06:49 +0300

    cccc

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -13,26 +13,17 @@ if [[ "$file" ]];
 then 
 policy=$(echo $file | awk -F '-' '{print $3}' | awk -F '.' '{print $1}' )
 policy=default-layer3-section
-# echo "========================================================================================"
-# echo -e "Processing flow of policy $policy and removing duplicates: "
-# echo "========================================================================================"
-# for i in $(cat $file |  grep -v "name,Protocol,Port" |  awk -F ']' '{print $2}' | grep  CATCH_*  | sed 's/ //g' | sed 's/"//g' | sort | uniq  );
-# do rule=$(echo $i | awk  -F ',' '{for(i=3; i<=NF; i++) {print $i}}' | grep  CATCH_* | sed 's/CATCH_//g');
-# echo $i
-# # echo "rule=$rule" ;
-# protocap=$(echo $i | awk -F ',' '{print $2}');
-# # echo "protocap=$protocap" ;
-# destport=$(echo $i | awk -F ',' '{print $3}') ; 
-# # echo "destport=$destport" ;
-# for t in $(echo $rule); do flow=$destport"*"$protocap"*"$t"*""\n"$flow ; done 
-# done
-# exit 1
-# echo "========================================================================================" ;
-# echo -e "\033[1;32mNon Zero Rules: \033[0m" ;
-# echo "========================================================================================" ;
-# echo -e $flow | sed '/^$/d' | awk -F '*' '{print $3}' | sort | uniq 
+non_zero_list=''
+for i in $(echo $rule_list )
+do if [[ $(grep -w $i $file) ]]; then non_zero_list="$non_zero_list $i"
+done 
+echo "========================================================================================" ;
+echo -e "\033[1;32mNon Zero Rules: \033[0m" ;
+echo "========================================================================================" ;
+echo -e non_zero_list | sed 's/CATCH_//g' | tr ' ' '\n'
 
-for i in $(echo $rule_list |  sed 's/CATCH_//g' ); 
+
+for i in $(echo $non_zero_list |  sed 's/CATCH_//g' ); 
 do 
 # if [[ "$i" == "INTEGR_APP_TO_INTRA" ]]
 # then
