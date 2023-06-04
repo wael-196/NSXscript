b98ee4ba37103a784dc0c508e961ccb86a9d5ae0
Wael_196 <Waelessam18@gmail.com>
2023-06-04 19:51:44 +0300

    dddd

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -220,9 +220,7 @@ echo -e $services | sed 's+/infra/services/++g'
 services=$lastservices" "$services
 total_service=$(echo $services | sed 's/,//g' | sed 's/ /, /g' )
 services="\"services\" : [ $total_service ],"
-echo $services
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$new_rule  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$services+" )
-echo $newjson
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$new_rule -s -d "$newjson" --header "Content-Type: application/json" )
 echo "========================================================================================"
 echo -e "\033[1;32mNew services associated with rule $new_rule : \033[0m"
