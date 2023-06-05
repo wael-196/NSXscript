7fe279b8a4eaa98d74e2bb4db1baad8440b9c42e
Wael_196 <Waelessam18@gmail.com>
2023-06-05 14:24:39 +0300

    csdcsd

--- adding_services_CSV.sh
+++ adding_services_CSV.sh
@@ -47,7 +47,7 @@ fi
 newservices='';
 
 
-old_ranges=$(echo -e $services | sed 's+/infra/services/++g' | sed 's+,++g' | sed 's+"++g' | tr ' ' '\n' | grep "[0-9]-[0-9]" | tr '\n' ' '  | sed 's+R_++g' )
+old_ranges=$(echo -e $services | sed 's+/infra/services/++g' | sed 's+,++g' | sed 's+"++g' | sed 's+R_++g' | tr ' ' '\n' | grep "[0-9]-[0-9]" | tr '\n' ' '   )
 
 echo old_ranges $old_ranges
 
