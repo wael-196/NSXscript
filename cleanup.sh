7ce4f88a9cc5296f2b69d17ae3c4dfb7575a4ec7
Wael_196 <Waelessam18@gmail.com>
2023-06-07 01:32:13 +0300

    sss

--- cleanup.sh
+++ cleanup.sh
@@ -8,23 +8,23 @@ rule_list="CATCH_APP_TO_INET CATCH_CLOSE_TO_NEAR CATCH_INTEGR_APP_TO_EXTRA CATCH
 adding_services(){
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$2+" )
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1 -s -d "$newjson" --header "Content-Type: application/json" )
-return $result
-}
-
-for i in $(echo $rule_list |  sed 's/CATCH_//g' ); 
-do
-services="\"services\" : [ \"\/infra\/services\/TCP_65535\" ],"
-result=$(adding_services $i $services)
 if [[ -z $(echo $result | grep "\"services\" :" ) ]] ; 
 then 
 echo -e "\033[1;31mCannot get services, something went wrong ! \033[0m"; 
 echo -e $result  ;
 else  
 echo "========================================================================================"
-echo -e "\033[1;32mNew services associated with rule $i : \033[0m"
+echo -e "\033[1;32mNew services associated with rule $1 : \033[0m"
 echo "========================================================================================"
 echo $result | awk -F '"services" : \\[' '{print $2}' | awk -F ']' '{print $1}' | sed 's+/infra/services/++g'
 fi
+}
+
+for i in $(echo $rule_list |  sed 's/CATCH_//g' ); 
+do
+services="\"services\" : [ \"\/infra\/services\/TCP_65535\" ],"
+result=$(adding_services $i $services)
+
 done 
 
 
