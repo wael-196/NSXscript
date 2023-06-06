8bc9aed272c2f7086fdd3a8947b234eed7ccbcef
Wael_196 <Waelessam18@gmail.com>
2023-06-06 12:23:34 +0300

    ss

--- Modifying_rule_action.sh
+++ Modifying_rule_action.sh
@@ -6,6 +6,7 @@ keyword="CATCH_"
 policy=$1
 action=\"$2\"
 rules=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep $keyword)
+echo Rules this are going to be changed $rules
 for i in $(echo $rules ) ; 
 do newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"action\" :.*+\"action\" : $action ,+" );
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" );
