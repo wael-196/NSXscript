1ff690c42fc486828d4d5ead8cd59117a5a0dd61
Wael_196 <Waelessam18@gmail.com>
2023-06-07 01:47:40 +0300

    ss

--- cleanup.sh
+++ cleanup.sh
@@ -4,17 +4,16 @@ user="admin"
 password="VMware1!VMware1!"
 policy=default-layer3-section
 rule_list="CATCH_APP_TO_INET CATCH_CLOSE_TO_NEAR CATCH_INTEGR_APP_TO_EXTRA CATCH_INTEGR_APP_TO_INTRA CATCH_INTEGR_EXTRA_TO_APP CATCH_INTEGR_INTRA_TO_APP CATCH_INTRA_APP CATCH_INTRA_NEAR CATCH_NEAR_TO_FAR"
-
+result=''
 adding_services(){
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$2+" )
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1 -s -d "$newjson" --header "Content-Type: application/json" )
-return $result
 }
 
 for i in $(echo $rule_list |  sed 's/CATCH_//g' ); 
 do
 services="\"services\" : [ \"\/infra\/services\/TCP_65535\" ],"
-result=$(adding_services "$i" "$services")
+adding_services "$i" "$services"
 if [[ -z $(echo $result | grep "\"services\" :" ) ]] ; 
 then 
 echo -e "\033[1;31mCannot get services, something went wrong ! \033[0m"; 
