9d2b3cf91c24c44cc403bacc5e37df4431a83e7b
Wael_196 <Waelessam18@gmail.com>
2023-06-07 01:34:06 +0300

    ss

--- cleanup.sh
+++ cleanup.sh
@@ -6,6 +6,10 @@ policy=default-layer3-section
 rule_list="CATCH_APP_TO_INET CATCH_CLOSE_TO_NEAR CATCH_INTEGR_APP_TO_EXTRA CATCH_INTEGR_APP_TO_INTRA CATCH_INTEGR_EXTRA_TO_APP CATCH_INTEGR_INTRA_TO_APP CATCH_INTRA_APP CATCH_INTRA_NEAR CATCH_NEAR_TO_FAR"
 
 adding_services(){
+echo $1
+
+echo $2
+
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1  -H "Accept: application/json" -s | sed "s+\"services\" :.*+$2+" )
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$1 -s -d "$newjson" --header "Content-Type: application/json" )
 if [[ -z $(echo $result | grep "\"services\" :" ) ]] ; 
