fe9e38b41a91b610afcc4c1ec65797c151532843
Wael_196 <Waelessam18@gmail.com>
2023-06-07 19:46:49 +0300

    last good point

--- adding.csv.sh
+++ adding.csv.sh
@@ -131,9 +131,6 @@ getting_services "$l"
 services=$services" "$getting_services_return
 done
 
-echo $services
-exit 1
-
 old_ranges=$(echo -e $services | sed 's+/infra/services/++g' | sed 's+,++g' | sed 's+"++g' | sed 's+R_++g' | tr ' ' '\n' | grep "[0-9]-[0-9]" | tr '\n' ' '   )
 
 Ranges=$(cat $file |  grep -v "name,Protocol,Port" |  awk -F ']' '{print $2}' | grep CATCH_ | sed 's/CATCH_//g' | grep -w $i | grep "[0-9]-[0-9]" |  awk -F ',' '{print $3"_"$2}' | sort -n | uniq  |  awk -F '_' '{print $2"_"$1}' ) ;
