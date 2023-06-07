9c31712abf27e6d5fda831aed4bdf3db714e4ddd
Wael_196 <Waelessam18@gmail.com>
2023-06-08 00:06:50 +0300

    last good point

--- adding.csv.sh
+++ adding.csv.sh
@@ -86,7 +86,7 @@ cleanup_of_ranges(){
 
 checking_related_services(){
 
-    checking_related_services_return=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep -w $1"-"[0-9] | sort )
+    checking_related_services_return=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep -w "$1"-"[0-9]\|$1"_"[0-9]" | sort )
 }
 
 if [[ "$file" ]];
