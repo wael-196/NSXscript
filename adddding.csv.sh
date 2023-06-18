a6e8319e74a23893897b6f6e58d7d61b3ef4d3ea
Wael_196 <Waelessam18@gmail.com>
2023-06-18 16:40:31 +0300

    rrr rrr

--- adddding.csv.sh
+++ adddding.csv.sh
@@ -9,6 +9,7 @@ getting_services_return=''
 cleanup_of_ranges_return=''
 checking_related_services_return=''
 
+echo $file
 cleanup_of_ranges(){
     local Range=$1
     for z in $(echo $Range) ; do 
