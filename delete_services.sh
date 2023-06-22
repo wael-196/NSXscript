e4f5e10ba4e3461811f1ea4dcc252bf8a7ff13a9
Wael_196 <Waelessam18@gmail.com>
2023-06-22 17:04:12 +0300

    sss

--- delete_services.sh
+++ delete_services.sh
@@ -4,8 +4,7 @@ user="admin"
 password="VMware1!VMware1!"
 
 
-services=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/services/ -s  |  grep -A 3 \"resource_type\"  |  grep \"id\" | awk -F ':' '{print $2}' | sed 's/"//g'  | sed 's/,//g')
-
+services=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/services/ -s  |  grep -A 3 \"resource_type\"  
 
 # for i in $(echo $services)
 # do curl -u $user:$password -k -X DELETE https://$fqdn/policy/api/v1/infra/services/$i -s
