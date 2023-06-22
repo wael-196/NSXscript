e854598625df4135a2a4672d0a9ad8de237a9125
Wael_196 <Waelessam18@gmail.com>
2023-06-22 23:53:40 +0300

    sss

--- delete_services.sh
+++ delete_services.sh
@@ -6,7 +6,8 @@ services=''
 read -e -i "$u" -p "Please enter U " input
 u="${input:-$u}"
 for i in {0..$u} ; do
-services=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/services?cursor=0004"$u"000 -s |  grep -A 1 "\"resource_type\" : \"Service\"" | grep \"id\" | grep "[T,U][C,D]P_[0-9]"  | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' )" "$services
+new=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/services?cursor=0004"$u"000 -s |  grep -A 1 "\"resource_type\" : \"Service\"" | grep \"id\" | grep "[T,U][C,D]P_[0-9]"  | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' )
+services=$services" "$new
 done
 
 echo $services 
