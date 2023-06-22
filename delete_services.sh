6c094c1f551cf4794122adad742081905dfcf151
Wael_196 <Waelessam18@gmail.com>
2023-06-22 23:06:52 +0300

    ss

--- delete_services.sh
+++ delete_services.sh
@@ -13,7 +13,7 @@ password="VMware1!VMware1!"
 
 
 
-for x in [1000-100000]
+for x in {1000..100000}
 do
  curl -u $user:$password -k -X PATCH "https://$fqdn/policy/api/v1/infra/services/TCP_$x" -s -d '{"display_name": "TCP_'$x'","_revision": 0,"service_entries": [{"resource_type": "L4PortSetServiceEntry","display_name": "tcp-ports","destination_ports": ["'$x'"],"l4_protocol": "TCP"}]}' --header "Content-Type: application/json"
 done
