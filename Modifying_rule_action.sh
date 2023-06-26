e241b23da96d42c1c42f022738ea68b9e93f5520
Wael_196 <Waelessam18@gmail.com>
2023-06-26 16:32:45 +0300

    sss

--- Modifying_rule_action.sh
+++ Modifying_rule_action.sh
@@ -14,8 +14,7 @@ respone="${input:-$respone}"
 if [[ "$respone" == "Y" ]]
 then
 for i in $(echo $rules ) ; 
-do 
-tag=$rules
+do tag=$i
 newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s |  sed "s+\"tag\" :.*++" | sed "s+\"disabled\" :.*+\"disabled\" : $action , \"tag\" : \"$tag\" ,+"  );
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" );
 if [[ -z $(echo $result | grep "\"disabled\" :" ) ]] ; 
@@ -27,8 +26,10 @@ else
 echo "========================================================================================"
 echo -e "\033[1;32m New action associated with rule $i : \033[0m"
 echo "========================================================================================"
-echo $result | awk -F '"disabled" : ' '{print $2}' | awk -F ',' '{print $1}'
-echo $result | awk -F '"tag" : ' '{print $2}' | awk -F ',' '{print $1}'
+disabled=$(echo $result | awk -F '"disabled" : ' '{print $2}' | awk -F ',' '{print $1}')
+tag2=$(echo $result | awk -F '"tag" : ' '{print $2}' | awk -F ',' '{print $1}')
+echo disabled=$disabled; 
+echo tag=$tag2 
 fi
 done
 fi
