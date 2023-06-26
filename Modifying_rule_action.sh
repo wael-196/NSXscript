2a94a9c399321f949d292ae1c70abc0cbf1746b5
Wael_196 <Waelessam18@gmail.com>
2023-06-26 14:23:01 +0300

    sss

--- Modifying_rule_action.sh
+++ Modifying_rule_action.sh
@@ -2,8 +2,9 @@
 fqdn="192.168.0.42"
 user="admin"
 password="VMware1!VMware1!"
-keyword="CATCH_"
+keyword="INTEGR_"
 policy=$1
+policy=default-layer3-section
 action=\"$2\"
 rules=$( curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/ -s | grep "\"id\"" | awk -F ': "' '{print $2}' | awk -F '",' '{print $1}' | grep $keyword)
 echo -e "\033[1;31mThese Rules are going to be changed\033[0m" 
@@ -12,9 +13,10 @@ read -e -i "$respone" -p "Please enter <Y> to accept " input
 respone="${input:-$respone}"
 if [[ "$respone" == "Y" ]]
 then
-tag=wael
 for i in $(echo $rules ) ; 
-do newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s | sed "s+\"disabled\" :.*+\"disabled\" : $action,+" | sed "s+\"disabled\" :.*+\"tag\" : $tag,+" );
+do 
+tag=$rules
+newjson=$(curl -u $user:$password -k -X GET https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i  -H "Accept: application/json" -s |  sed "s+\"tag\" :.*++" | sed "s+\"disabled\" :.*+\"disabled\" : $action , \"tag\" : \"$tag\" ,+"  );
 result=$(curl -u $user:$password -k -X PUT https://$fqdn/policy/api/v1/infra/domains/default/security-policies/$policy/rules/$i -s -d "$newjson" --header "Content-Type: application/json" );
 if [[ -z $(echo $result | grep "\"disabled\" :" ) ]] ; 
 then 
@@ -25,7 +27,8 @@ else
 echo "========================================================================================"
 echo -e "\033[1;32m New action associated with rule $i : \033[0m"
 echo "========================================================================================"
-echo $result | awk -F '"action" : ' '{print $2}' | awk -F ',' '{print $1}'
+echo $result | awk -F '"disabled" : ' '{print $2}' | awk -F ',' '{print $1}'
+echo $result | awk -F '"tag" : ' '{print $2}' | awk -F ',' '{print $1}'
 fi
 done
 fi
